a53241efcf03e4ac18541e086ada7fc474e0adb0
Ivan Mic <ivan.micr@gmail.com>
2019-04-11 18:01:31 +0300

    further development

--- src/main/java/ru/imikryakov/mymusic/musicscanner/scanner/TrackVisitor.java
+++ src/main/java/ru/imikryakov/mymusic/musicscanner/scanner/TrackVisitor.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class TrackVisitor extends SimpleFileVisitor<Path> {
     private static Logger logger = LoggerFactory.getLogger(TrackVisitor.class);
+
+    private PathMatcher MP3_MATCHER = FileSystems.getDefault().getPathMatcher("glob:*.mp3");
 
     private ScanResult result;
 
@@ -21,6 +21,10 @@ public class TrackVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
         logger.info("visiting file {}", path);
+        if (!MP3_MATCHER.matches(path.getFileName())) {
+            logger.info("not an MP3! skip");
+            return FileVisitResult.CONTINUE;
+        }
         result.addTrack(new TrackInfo(path));
         return FileVisitResult.CONTINUE;
     }
